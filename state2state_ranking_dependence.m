d1ae3cd594146f460f83525861399cc52349dcab
paolobaruchspinoza <baruccap@gmail.com>
2016-04-25 17:09:54 +0100

    Update state2state_ranking_dependence.m

--- state2state_ranking_dependence.m
+++ state2state_ranking_dependence.m
@@ -1,16 +1,17 @@
 %state to state ranking dependence analysis
+% data_tensor is a three index tensor TxNxM of non-negative numbers 
+function [ranking_dependence_tensor overlap_set_tensor] = state2state_ranking_depende(filename, kmax)
+load('filename')
+nM = size(data_tensor,3);
 
-load('fUsaMat_freq.mat')
+ranking_dependence_tensor = zeros([nM nM kmax]);
+overlap_set_tensor = zeros([nM nM kmax]);
 
-kmax = 1;
-ranking_dependence_tensor = zeros([51 51 kmax]);
-overlap_set_tensor = zeros([51 51 kmax]);
-
-for s1 = 1:51
-    Xlong = fUsaMat_freq(:,:,s1);
+for s1 = 1:nM
+    Xlong = data_tensor(:,:,s1);
     X = (Xlong(:,sum(Xlong,1)>0))';
-    for s2 = s1+1:51 
-        Ylong = fUsaMat_freq(:,:,s2);
+    for s2 = s1+1:nM
+        Ylong = data_tensor(:,:,s2);
         Y = (Ylong(:,sum(Ylong,1)>0))';
         for k =1:kmax
             [r ov] = ranking_dependence(X,Y,k);
@@ -18,7 +19,7 @@ for s1 = 1:51
             overlap_set_tensor(s1,s2,k) = ov;  
             overlap_set_tensor(s2,s1,k) = ov;  
             if ov > 0
-            ranking_dependence_tensor(s2,s1,k) = 1-r; 
+            ranking_dependence_tensor(s2,s1,k) = -r; 
             end
         end
     end
